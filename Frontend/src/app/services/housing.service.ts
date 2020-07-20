f02e035f29b091c7699908f454855939fae5a9aa
osama-moradm <osama.m.mabrouk@gmail.com>
2020-07-20 14:41:00 +0300

    Add multiple properties

--- Frontend/src/app/services/housing.service.ts
+++ Frontend/src/app/services/housing.service.ts
@@ -16,6 +16,16 @@ export class HousingService {
     return this.http.get('data/properties.json').pipe(
       map(data => {
         const propertyArray: Array<IProperty> = [];
+        const newProperties = JSON.parse(localStorage.getItem('newProp'));
+        if (newProperties){
+          for (const id in newProperties)
+          {
+            if (data.hasOwnProperty(id) && newProperties[id].SellRent === sellRent)
+            {
+              propertyArray.push(newProperties[id]);
+            }
+          }
+          }
         for (const id in data)
         {
           if (data.hasOwnProperty(id) && data[id].SellRent === sellRent)
@@ -29,6 +39,21 @@ export class HousingService {
   }
 
   addProperty(property: Property) {
-    localStorage.setItem('newProp', JSON.stringify(property));
+    let newProp = [property];
+    if (localStorage.getItem('newProp')){
+      newProp = [property, ...JSON.parse(localStorage.getItem('newProp'))];
+    }
+    localStorage.setItem('newProp', JSON.stringify(newProp));
+  }
+
+  newPropID(){
+    var pID = localStorage.getItem('PID');
+    if (pID){
+      localStorage.setItem('PID', String(+pID + 1));
+      return (+pID + 1);
+    }else{
+      localStorage.setItem('PID', '101');
+      return 101;
+    }
   }
 }
